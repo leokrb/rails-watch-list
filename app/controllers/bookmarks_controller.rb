d42281c1d18e3243d8a34406635090e0ad95fa83
Rowena Koerber <rowenakoerber@gmail.com>
2021-04-30 22:14:54 +0200

    bookmark controller and show

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -17,6 +17,10 @@ class BookmarksController < ApplicationController
   end
 
   def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+
+    redirect_to list_path(@bookmark.list)
   end
 
   private
